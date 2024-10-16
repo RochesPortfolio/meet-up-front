369c9cac380914643143bb79eb6d796589566b8c
Zucely Hernández <65913550+anzucel@users.noreply.github.com>
2024-10-16 00:23:18 -0600

    feat(Guest.js): connecting with db

--- src/pages/Guest.js
+++ src/pages/Guest.js
@@ -1,58 +1,61 @@
 // Filename - pages/about.js
 
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './Guest.css';
-import { Select } from "antd";
+import { Select, Spin } from "antd";
 import { Table, Card, Col, Row, Statistic, Tag, Button } from "antd";
 
 const Guest = () => {
-    const events = ['Evento A', 'Evento B', 'Evento C'];
-    const states = ['Aceptado', 'Pendiente', 'Rechazado'];
-
+    const [events, setEvents] = useState([]);
     const [eventSelected, setEventSelected] = useState(undefined);
-    const [stateSelected, setStateSelected] = useState('');
+    const [stateSelected] = useState('');
+    const [listGuest, setListGuest] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    const listGuest = [
-        {
-          nombre: 'Juan',
-          apellido: 'Pérez',
-          correo: 'juan.perez@example.com',
-          telefono: '1234567890',
-          genero: 'Masculino',
-          empresa: 'Empresa A',
-          negocio: 'Tecnología',
-          fechaInvitacion: '2024-09-30',
-          fechaConfirmacion: '2024-10-01',
-          estadoInvitacion: 'Aceptado',
-          evento: 'Evento A'
-        },
-        {
-            nombre: 'María',
-            apellido: 'López',
-            correo: 'maria.gonzalez@example.com',
-            telefono: '0987654321',
-            genero: 'Femenino',
-            empresa: 'Empresa B',
-            negocio: 'Marketing',
-            fechaInvitacion: '2024-09-29',
-            fechaConfirmacion: '2024-10-02',
-            estadoInvitacion: 'Pendiente',
-            evento: 'Evento B'
-        },
-        {
-          nombre: 'Sofía',
-          apellido: 'González',
-          correo: 'maria.gonzalez@example.com',
-          telefono: '0987654321',
-          genero: 'Femenino',
-          empresa: 'Empresa B',
-          negocio: 'Marketing',
-          fechaInvitacion: '2024-09-29',
-          fechaConfirmacion: '2024-10-02',
-          estadoInvitacion: 'Rechazado',
-          evento: 'Evento B'
+    const fetchEvents = async () => {
+        try {
+            const res = await fetch('http://localhost:3030/api/events');
+            const data = await res.json();
+            console.log(data);
+            const eventList = data.map(event => event.nombre_evento);
+            setEvents(eventList);
+        } catch (error) {
+            console.error("Error al obtener lista de eventos:", error);
+            setLoading(false)
         }
-      ];
+    }
+
+    const fetchGuest = async () => {
+        try {
+            const res = await fetch('http://localhost:3030/api/guests');
+            const data = await res.json();
+            console.log(data)
+            const mappedData = data.map(guest => ({
+                nombre: guest.id_persona.nombres,
+                apellido: guest.id_persona.apellidos,
+                correo: guest.id_persona.correo,
+                telefono: guest.id_persona.telefono,
+                genero: guest.id_persona.genero === 'M' ? 'Masculino' : 'Femenino',
+                empresa: guest.id_empresa.nombre || 'Empresa Desconocida',
+                negocio: guest.id_empresa.rubro_negocio,
+                fechaInvitacion: guest.fecha_invitacion,
+                fechaConfirmacion: guest.fecha_confirmacion || '-',
+                estadoInvitacion: guest.estado_invitacion,
+                evento: guest.id_evento.nombre_evento
+            }));
+
+            setListGuest(mappedData);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error al obtener lista de asistentes:", error);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchEvents();
+        fetchGuest();
+    }, []);
 
     const filterGuest = listGuest.filter(guest => {
         const event = eventSelected ? guest.evento === eventSelected: true;
@@ -64,7 +67,7 @@ const Guest = () => {
     const uniqueBusiness = [...new Set(listGuest.map(business => business.negocio))];
     const uniqueState = [...new Set(listGuest.map(state => state.estadoInvitacion))];
 
-    const accepted = filterGuest.filter(guest => guest.estadoInvitacion ===  'Aceptado').length;
+    const accepted = filterGuest.filter(guest => guest.estadoInvitacion ===  'Confirmada').length;
     const pending = filterGuest.filter(guest => guest.estadoInvitacion ===  'Pendiente').length;
     const rejected = filterGuest.filter(guest => guest.estadoInvitacion ===  'Rechazado').length;
 
@@ -110,7 +113,7 @@ const Guest = () => {
             render: (estado) => {
               let color = '';
               switch (estado) {
-                case 'Aceptado':
+                case 'Confirmada':
                   color = 'green';
                   break;
                 case 'Pendiente':
@@ -128,91 +131,99 @@ const Guest = () => {
     ]
 
 	return (
-		<div>
-			<h1>Asistentes por evento</h1>
-            <div className="tableContainer">
-                <table className="tableContent">
-                    <thead>
-                        <tr>
-                            <th>
-                                {/* Muestra asistentes filtrados por evento */}
-                                <div>
-                                    <label htmlFor="event" className="labelContent">Selecciona un evento:</label>
-                                    <Select 
-                                    value={eventSelected}
-                                    onChange={(value) => setEventSelected(value)}
-                                    placeholder="-- Selecciona --"
-                                    style={{width: 180}}
-                                    >
-                                    {events.map((event, index) => (
-                                        <Select.Option key={index} value={event}>{event}</Select.Option>
-                                    ))}
-                                    </Select>
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>      
-            <div style={{ marginTop: '20px' }}>
-                <Button onClick={cleanSelection}>Limpiar Selección</Button>
-            </div>      
-            <div>
-                <h2>Lista de Asistentes</h2>
-                <div className="rowContainer">
-                    <Row gutter={16} className="rowContent">
-                        <Col span={3}>
-                            <Card bordered={true}>
-                                <Statistic
-                                    title="Aceptados"
-                                    value={accepted}
-                                    precision={0}
-                                    valueStyle={{
-                                        color: '#3f8600',
-                                    }}
-                                    prefix=""
-                                />
-                            </Card>
-                        </Col>
-                        <Col span={3}>
-                            <Card bordered={true}>
-                                <Statistic
-                                    title="Pendientes"
-                                    value={pending}
-                                    precision={0}
-                                    valueStyle={{
-                                        color: '#f0e512',
-                                    }}
-                                    prefix=""
-                                />
-                            </Card>
-                        </Col>
-                        <Col span={3}>
-                            <Card bordered={true}>
-                                <Statistic
-                                    title="Rechazados"
-                                    value={rejected}
-                                    precision={0}
-                                    valueStyle={{
-                                        color: '#cf1322',
-                                    }}
-                                    prefix=""
-                                />
-                            </Card>
-                        </Col>
-                    </Row>
+        <div>
+            {loading ? (
+                <div className="spin-container">
+                    <Spin size="large" fullscreen/>
                 </div>
-                <div className="bodyContainer">
-                {filterGuest.length > 0 ? (
-                    <Table dataSource={filterGuest} columns={columns} 
-                    rowKey={(record, index) => index} />
-                ) : (
-                    (eventSelected || stateSelected) == "" ? <h3></h3> :
-                    <h3>No existe información</h3>
-                )}
+            ) : (
+                <div>         
+                    <h1>Asistentes por evento</h1>
+                    <div className="tableContainer">
+                        <table className="tableContent">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        {/* Muestra asistentes filtrados por evento */}
+                                        <div>
+                                            <label htmlFor="event" className="labelContent">Selecciona un evento:</label>
+                                            <Select 
+                                            value={eventSelected}
+                                            onChange={(value) => setEventSelected(value)}
+                                            placeholder="-- Selecciona --"
+                                            style={{width: 180}}
+                                            >
+                                            {events.map((event, index) => (
+                                                <Select.Option key={index} value={event}>{event}</Select.Option>
+                                            ))}
+                                            </Select>
+                                        </div>
+                                    </th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>      
+                    <div style={{ marginTop: '20px' }}>
+                        <Button onClick={cleanSelection}>Limpiar Selección</Button>
+                    </div>      
+                    <div>
+                        <h2>Lista de Asistentes</h2>
+                        <div className="rowContainer">
+                            <Row gutter={16} className="rowContent">
+                                <Col span={3}>
+                                    <Card bordered={true}>
+                                        <Statistic
+                                            title="Confirmada"
+                                            value={accepted}
+                                            precision={0}
+                                            valueStyle={{
+                                                color: '#3f8600',
+                                            }}
+                                            prefix=""
+                                        />
+                                    </Card>
+                                </Col>
+                                <Col span={3}>
+                                    <Card bordered={true}>
+                                        <Statistic
+                                            title="Pendientes"
+                                            value={pending}
+                                            precision={0}
+                                            valueStyle={{
+                                                color: '#f0e512',
+                                            }}
+                                            prefix=""
+                                        />
+                                    </Card>
+                                </Col>
+                                <Col span={3}>
+                                    <Card bordered={true}>
+                                        <Statistic
+                                            title="Rechazados"
+                                            value={rejected}
+                                            precision={0}
+                                            valueStyle={{
+                                                color: '#cf1322',
+                                            }}
+                                            prefix=""
+                                        />
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </div>
+                        <div className="bodyContainer">
+                        {filterGuest.length > 0 ? (
+                            <Table dataSource={filterGuest} columns={columns} 
+                            rowKey={(record, index) => index} />
+                        ) : (
+                            (eventSelected || stateSelected) === " " ? <h3></h3> :
+                            <h3>No existe información</h3>
+                        )}
+                        </div>
+                    </div>
                 </div>
-            </div>
-		</div>
+            )}
+        </div>
 	);
 };
 
