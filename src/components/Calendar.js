592a2343a645ba8b937469fe0c5c5d6efe8a32b4
Cool233777 <46784877+Cool233777@users.noreply.github.com>
2024-10-24 14:36:00 -0600

    refactor(Calendar.js): Modificacion de pantalla
    Se unificó el form y el calendar para manejar la actualizacion de eventos al crearse, se mejoro la experiencia de usuario.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -8,10 +8,10 @@ import { Form } from "antd";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import EventForm from './EventForm';
+import { Snackbar } from '@material-ui/core';
 import '../components/EventForm.css';
 import EventDetails from './EventDetails';
 import EventSimple from './EventSimple';
-import { Widgets } from '@material-ui/icons';
 
 const localizer = momentLocalizer(moment);
 
@@ -26,6 +26,14 @@ const MyCalendar = () => {
 
     const [form] = Form.useForm();
 
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+
+    const handleCloseSnackbar = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenSnackbar(false);
+    };
 
     const handleAddEvent = () => {
         setShowForm(true);
@@ -38,6 +46,55 @@ const MyCalendar = () => {
     const handleEventCreated = (newEvent) => {
         setEvents([...events, newEvent]);
         setShowForm(false); // Cerrar el formulario una vez que se crea el evento
+    };
+
+    const [formData, setFormData] = useState({
+        nombre_evento: '',
+        lugar_evento: '',
+        aforo_evento: '',
+        tipo_evento: '',
+        descripcion: '',
+        rubro_negocio: '',
+        hora_inicio: '',
+        hora_culminacion: '',
+        fecha_inicio: '',
+        fecha_finalizacion: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const res = await fetch('http://localhost:3030/api/events/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            if (res.ok) {
+                const newEvent = await res.json();
+                handleEventCreated(newEvent);
+                setOpenSnackbar('Evento creado con éxito');
+                await fetchEvents();
+            } else {
+                setOpenSnackbar('Error al crear el evento');
+            }
+        } catch (error) {
+            console.error('Error al crear el evento:', error);
+        }
+        finally {
+            setLoading(false);
+        }
     };
 
     const fetchEvents = async () => {
@@ -92,6 +149,12 @@ const MyCalendar = () => {
 
     return (
         <div className="calendar-container">
+            <Snackbar
+                open={openSnackbar}
+                autoHideDuration={3000}
+                onClose={handleCloseSnackbar}
+                message="Evento creado con éxito"
+            />
             <div className="header-container">
                 <h1>Calendario de Eventos</h1>
                 <div>
@@ -129,7 +192,128 @@ const MyCalendar = () => {
 
             {showForm && (
                 <div className="form-popup">
-                    <EventForm onEventCreated={handleEventCreated} />
+                    {/* <EventForm onEventCreated={handleEventCreated} /> */}
+                    <form onSubmit={handleSubmit} className="event-form">
+                        <div className="form-group">
+                            <label htmlFor="nombre_evento">Nombre del Evento</label>
+                            <input
+                                type="text"
+                                id="nombre_evento"
+                                name="nombre_evento"
+                                value={formData.nombre_evento}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="lugar_evento">Lugar del Evento</label>
+                            <input
+                                type="text"
+                                id="lugar_evento"
+                                name="lugar_evento"
+                                value={formData.lugar_evento}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="aforo_evento">Aforo del Evento</label>
+                            <input
+                                type="number"
+                                id="aforo_evento"
+                                name="aforo_evento"
+                                value={formData.aforo_evento}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="tipo_evento">Tipo de Evento</label>
+                            <input
+                                type="text"
+                                id="tipo_evento"
+                                name="tipo_evento"
+                                value={formData.tipo_evento}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="rubro_negocio">Rubro del negocio</label>
+                            <input
+                                type="text"
+                                id="rubro_negocio"
+                                name="rubro_negocio"
+                                value={formData.rubro_negocio}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="descripcion">Descripción</label>
+                            <textarea
+                                id="descripcion"
+                                name="descripcion"
+                                value={formData.descripcion}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="hora_inicio">Hora de Inicio</label>
+                            <input
+                                type="time"
+                                id="hora_inicio"
+                                name="hora_inicio"
+                                value={formData.hora_inicio}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="hora_culminacion">Hora de Culminación</label>
+                            <input
+                                type="time"
+                                id="hora_culminacion"
+                                name="hora_culminacion"
+                                value={formData.hora_culminacion}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="fecha_inicio">Fecha de Inicio</label>
+                            <input
+                                type="date"
+                                id="fecha_inicio"
+                                name="fecha_inicio"
+                                value={formData.fecha_inicio}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="fecha_finalizacion">Fecha de Finalización</label>
+                            <input
+                                type="date"
+                                id="fecha_finalizacion"
+                                name="fecha_finalizacion"
+                                value={formData.fecha_finalizacion}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        <button className='form-group' type="submit">Crear Evento</button>
+                    </form>
                     <Button onClick={handleCloseForm}>Cerrar</Button>
                 </div>
             )}
