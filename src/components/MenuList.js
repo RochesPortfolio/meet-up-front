db5944d8d58f47c789e4f838a007b83e290e6a1d
Zucely Hernández <65913550+anzucel@users.noreply.github.com>
2024-10-24 23:23:08 -0600

    creating new menu item in menuList

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -3,7 +3,7 @@ import { Menu } from "antd";
 import { HomeOutlined } from "@material-ui/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarMinus } from '@fortawesome/free-regular-svg-icons';
-import { faHouse, faPeopleGroup, faGaugeHigh } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faPeopleGroup, faGaugeHigh, faListCheck } from "@fortawesome/free-solid-svg-icons";
 import { Link, useLocation } from "react-router-dom";
 
 const MenuList = ({darkTheme}) => {
@@ -12,22 +12,27 @@ const MenuList = ({darkTheme}) => {
     const optionsMenu = {
         '/home': 'home',
         '/guest': 'guests',
-        '/events': 'event'
+        '/event': 'events',
+        '/calendar': 'calendar',
     }
 
     return (
         <Menu theme={darkTheme ? 'dark' : 'light'} mode="inline" className="menu-bar" selectedKeys={[optionsMenu[location.pathname] || 'home']}>
             <Menu.Item key="home" icon={<FontAwesomeIcon icon={faGaugeHigh} />}>
                 Dashboard
-                <Link to="/home"></Link>
+                <Link to="/dashboard"></Link>
             </Menu.Item>
             <Menu.Item key="guests" icon={<FontAwesomeIcon icon={faPeopleGroup} />}>
                 Invitados
                 <Link to="/guest"></Link>
             </Menu.Item>
-            <Menu.Item key="events" icon={<FontAwesomeIcon icon={faCalendarMinus} />}>
-                Eventos
+            <Menu.Item key="events" icon={<FontAwesomeIcon icon={faListCheck} />}>
+                Lista de Eventos
                 <Link to="/event"></Link>
+            </Menu.Item>
+            <Menu.Item key="calendar" icon={<FontAwesomeIcon icon={faCalendarMinus} />}>
+                Calendario
+                <Link to="/calendar"></Link>
             </Menu.Item>
             {/* <Menu.SubMenu key="subtasks" icon={<HomeOutlined/>} title="Tasks">
                 <Menu.Item key="events" icon={<HomeOutlined/>}>
